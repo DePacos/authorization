17ffa2634ab76f66e5c4a555ea75aede7073840a
DePacos <depacos@yandex.ru>
2025-08-06 22:30:13 +0300

    feat: is dev config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { IS_DEV_ENV } from '@/utils/isDev';
+
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			ignoreEnvFile: !IS_DEV_ENV,
+		}),
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { RedisStore } from 'connect-redis';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import IORedis from 'ioredis';
+
+import { parseBoolean } from '@/utils/parseBoolean';
+import { ms, StringValue } from '@/utils/timeToMs';
 
 import { AppModule } from './app.module';
 
@@ -8,8 +15,36 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = app.get(ConfigService);
+	const redis = new IORedis(config.getOrThrow<string>('COOKIES_SECRET'));
 
-	app.useGlobalPipes(new ValidationPipe());
+	app.use(cookieParser(config.getOrThrow('COOKIES_SECRET')));
+	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+				httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+				secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+				sameSite: 'lax',
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER'),
+			}),
+		}),
+	);
+
+	app.enableCors({
+		origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+		credentials: true,
+		exposeHeaders: ['set-cookie'],
+	});
 
 	await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
