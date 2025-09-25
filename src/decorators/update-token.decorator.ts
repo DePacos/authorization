190231a4018cf398596739d7b999b3d3abf5d7c2
DePacos <depacos@yandex.ru>
2025-09-25 21:11:24 +0300

    feat: update token guard

---
+++ src/decorators/update-token.decorator.ts
@@ -0,0 +1,11 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+export const UpdateToken = createParamDecorator((_: unknown, context: ExecutionContext) => {
+	const request: Request = context.switchToHttp().getRequest();
+
+	return {
+		user: request.user,
+		tokenUuid: request.tokenUuid,
+	};
+});
---
+++ src/guards/update-token.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Request } from 'express';
+
+import { TokensService } from '@/auth/tokens/tokens.service';
+import { UserService } from '@/user/user.service';
+
+@Injectable()
+export class UpdateTokenGuard implements CanActivate {
+	public constructor(
+		private readonly userService: UserService,
+		private readonly tokenService: TokensService,
+	) {}
+
+	public async canActivate(context: ExecutionContext): Promise<boolean> {
+		const request: Request = context.switchToHttp().getRequest();
+
+		const refreshToken = request.cookies?.refreshToken as string | undefined;
+		if (!refreshToken) throw new UnauthorizedException('Token invalid');
+
+		const { foundTokenRow } = await this.tokenService.verifyRefreshToken(refreshToken);
+		const user = await this.userService.getUserById(foundTokenRow.userId);
+		if (!user) throw new NotFoundException('User not found');
+
+		request.user = user;
+		request.tokenUuid = foundTokenRow.tokenUuid;
+
+		return true;
+	}
+}
